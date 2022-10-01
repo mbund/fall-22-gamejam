954cbba12eec428c0b4cd8d8e5044f640713e8b5
josh <jm8hjkl@gmail.com>
2022-10-01 10:50:30 -0400

    new gravity

--- globulars.gd
+++ globulars.gd
@@ -1,5 +1,11 @@
 extends Node
 
 var player: Player
+var blackholes: Array[Blackhole]
 
-\
+func calculate_gravity(pos: Vector2) -> Vector2:
+	var gravity = Vector2.ZERO
+	for blackhole in blackholes:
+		var r = (blackhole.global_position - pos)
+		gravity += blackhole.gravity_strength * (r.normalized() / r.length_squared())
+	return gravity
--- player/player.gd
+++ player/player.gd
@@ -1,10 +1,9 @@
 class_name Player
-extends RigidBody2D
+extends CharacterBody2D
 
 const acceleration_strength = 1000
 const break_strength = acceleration_strength
 const rotation_strength = PI
-const blackhole_strength = 50
 const dec = 1
 
 @onready var exhaust: CanvasItem = $exhaust
@@ -13,20 +12,23 @@ const dec = 1
 func _ready():
 	Globulars.player = self
 
-func _integrate_forces(state):
+func _process(delta):
+	velocity += Globulars.calculate_gravity(global_position)
+	
 	if Input.is_action_pressed("accelerate"):
-		constant_force = acceleration_strength * transform.x
-	elif Input.is_action_pressed("brake"):
-		constant_force = -break_strength  * transform.x
+		velocity += acceleration_strength  * transform.x * delta
+		exhaust.visible = true
 	else:
-		constant_force = Vector2.ZERO
-	
-	exhaust.visible = Input.is_action_pressed("accelerate")
-	exhaustfront.visible = Input.is_action_pressed("brake")
-	
+		exhaust.visible = false
+	if Input.is_action_pressed("brake"):
+		velocity -= break_strength  * transform.x * delta
+		exhaustfront.visible = true
+	else:
+		exhaustfront.visible = false
 	if Input.is_action_pressed("left"):
-		angular_velocity = -rotation_strength
-	elif Input.is_action_pressed("right"):
-		angular_velocity = rotation_strength
-	else:
-		angular_velocity = 0
+		rotate(-rotation_strength * delta)
+	if Input.is_action_pressed("right"):
+		rotate(rotation_strength * delta)
+		
+	move_and_slide()
+	
