2066e71ee42938f74163a7fe7bb4798f90f83049
josh <jm8hjkl@gmail.com>
2022-10-01 15:57:15 -0400

    starfield

---
+++ environment/GpuParticles2d.gd
@@ -0,0 +1,5 @@
+extends GPUParticles2D
+
+func _draw() -> void:
+	pass
+	# draw_rect(visibility_rect, Color(1, 0, 0, 0.2))
---
+++ environment/starfield.gd
@@ -0,0 +1,9 @@
+extends Node
+
+@onready var colorRect: ColorRect = $CanvasLayer/ColorRect
+@onready var camera: Camera2D = $Camera2d
+@onready var particles: GPUParticles2D = $GpuParticles2d2
+
+func _process(delta):
+	colorRect.material.set_shader_parameter("pos", camera.position)
+	particles.position = camera.position
