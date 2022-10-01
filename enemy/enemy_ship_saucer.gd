e4d01af77e8485f1c26446c042c9bbfba41c3878
Christopher <christophermcd0903@gmail.com>
2022-10-01 10:37:15 -0400

    added teleporting saucer

---
+++ enemy/enemy_ship_saucer.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+
+var laser_scene = preload("res://enemy/laser.tscn");
+@export var turning_speed = 0.25;
+
+@export var target: Marker2D;
+
+
+func _on_gun_timer_timeout():
+	for node in $GunPositions.get_children():
+		var laser_direction = (node.global_position - $GunPositions.global_position).angle();
+		var laser: CharacterBody2D = laser_scene.instantiate();
+		laser.rotation = laser_direction;
+		laser.global_position = node.global_position;
+		add_sibling(laser);
+		
+
+func _physics_process(delta):
+	rotation += delta * turning_speed;
+
+
+func _on_teleport_timer_timeout():
+	if target == null:
+		return
+	global_position = Vector2(1, 0).rotated(target.rotation + PI) * randf_range(200, 400) + target.global_position
--- enemy/laser.gd
+++ enemy/laser.gd
@@ -1,11 +1,11 @@
 extends CharacterBody2D
 
 @export
-var speed = 1000
+var speed = 1500
 
-func _physics_process(_delta):
+func _physics_process(delta):
 	velocity = Vector2(speed, 0).rotated(self.rotation)
-	move_and_slide()
+	move_and_collide(velocity * delta)
 
 
 func _on_timer_timeout():
