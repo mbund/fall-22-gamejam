65aa1ccdfc80c06c204d3ece25993d7d4ea33796
mbund <25110595+mbund@users.noreply.github.com>
2022-09-30 21:06:53 -0400

    animate black hole

---
+++ environment/autogen.gd
@@ -0,0 +1,22 @@
+extends Node
+
+@export var num_black_holes = 4;
+
+var blackhole_scene = preload("res://environment/blackhole.tscn");
+
+const MEASURE = 500;
+
+func _ready():
+	for _i in range(num_black_holes):
+		var x = randf_range(0, MEASURE)
+		var y = randf_range(0, MEASURE)
+		
+		var blackhole: Sprite2D = blackhole_scene.instantiate()
+		blackhole.global_position = Vector2(x, y)
+		
+		add_child(blackhole)
+	
+	pass
+
+func _process(delta):
+	pass
