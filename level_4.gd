acc87845a83299f1ae7562ea62327c0dc3608964
mbund <25110595+mbund@users.noreply.github.com>
2022-10-02 17:52:49 -0400

    add level 4

---
+++ level_4.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+@onready
+var player_scene = load("res://player/player.tscn")
+
+
+func _ready():
+	Globulars.world = self
+	var player = player_scene.instantiate();
+	player.global_position = $player_spawn.global_position
+	get_viewport().get_camera_2d().global_position = player.global_position
+	add_child(player)
+
+func _process(_delta):
+	if !has_node("EnemyShip2") and !has_node("EnemyShip3") and !has_node("EnemyShip6") and !has_node("EnemyShip14") and !has_node("EnemyShip15") and !has_node("EnemyShip16"):
+		SceneTransition.change_scene("res://level_5.tscn")
+		set_process(false)
