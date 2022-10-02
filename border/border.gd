ec485509f6b265e718ebe73e40334d5f4ee964a0
josh <jm8hjkl@gmail.com>
2022-10-02 14:25:36 -0400

    notability selection

--- border/border.gd
+++ border/border.gd
@@ -1,7 +1,21 @@
 extends Area2D
 
+@export var setvelocity: Vector2
+
+var time = 0.0
+
+func _ready():
+	$ColorRect.material.set_shader_parameter("scalex", scale.x)
 
 func _process(delta):
+	time += delta
+	$ColorRect.material.set_shader_parameter("t", time)
+	
+
+func _physics_process(delta):
 	for body in get_overlapping_bodies():
 		if body is Player:
-			body.velocity.y = 300
+			if setvelocity.x != 0:
+				body.velocity.x = setvelocity.x
+			if setvelocity.y != 0:
+				body.velocity.y = setvelocity.y
