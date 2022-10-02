8fcaf191c6729edc58e1b1d366d5b943ae6452f3
josh <jm8hjkl@gmail.com>
2022-10-02 18:41:23 -0400

    music!

--- LossScreen.gd
+++ LossScreen.gd
@@ -3,5 +3,5 @@ extends Node2D
 
 func _ready():
 	Globulars.reset()
-	await get_tree().create_timer(3.0).timeout#=
-	SceneTransition.change_scene("res://mainmenu/menu.tscn")
+	await get_tree().create_timer(3.0).timeout
+	SceneTransition.change_scene(Globulars.current_level)
