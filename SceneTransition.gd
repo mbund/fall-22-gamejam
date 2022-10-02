0e461cf337821444dae42f52c2279a34b90428e7
josh <jm8hjkl@gmail.com>
2022-10-02 17:27:45 -0400

    bug fix - lives and stars

--- SceneTransition.gd
+++ SceneTransition.gd
@@ -4,5 +4,6 @@ func change_scene(path):
 	var anim: AnimationPlayer = $AnimationPlayer
 	anim.play("fade")
 	await anim.animation_finished
+	Globulars.reset()
 	get_tree().change_scene_to_file(path)
 	anim.play_backwards("fade")
